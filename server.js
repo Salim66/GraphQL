2f5f6a19baca39e49396e1dbde7bf83aa0478c4d
Salim66 <salimhasanriad@gmail.com>
2024-11-27 23:06:22 +0600

    Apollo Server GraphQL install and Apollo Server Configuration

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 import colors from 'colors';
 import dotenv from 'dotenv';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
 // confiq dot env
 dotenv.config();
 
-console.log("hello GraphQL")
+// init apollo server
+const server = new ApolloServer({});
+
+
+// run server
+const {url} = await startStandaloneServer(server, {
+    listen: {
+        port: 5050
+    }
+});
+
+console.log(`Apollo server is running ${url}`)
